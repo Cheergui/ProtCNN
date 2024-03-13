bd9bed6b95be5f61a8a20b86ca6ff9ec3e58d421
Cheergui <aminejelloul23@gmail.com>
2024-03-12 17:25:53 -0700

    Optimized the evaluation script

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
 from types import SimpleNamespace
+import click
 
 from models.protcnn import ProtCNN
 from data.datamodule import DataModule
@@ -8,7 +9,25 @@ from data.datamodule import DataModule
 
 from pytorch_lightning import Trainer
 
+root = Path(__file__).parent.parent
+json_path = root / 'params.json'
 
+with open(json_path, 'r') as file:
+        params = json.load(file, object_hook=lambda d: SimpleNamespace(**d))
+        
+# Extracting default values
+evaluation_params = params.evaluation
+# dataloader_params = params.dataloader
+
+
+@click.command()
+@click.option('--checkpoint-path', default=evaluation_params.checkpoint_path, type=str, help='Directory within the root folder containing the dataset.')
+# @click.option('--data-dir', default=root / dataloader_params.data_dir, type=str, help='Directory within the root folder containing the dataset.')
+# @click.option('--batch-size', default=dataloader_params.batch_size, type=int, help='Batch size for evaluation')
+# @click.option('--max-len', default=dataloader_params.max_len, type=int, help='Maximum sequence length')
+# @click.option('--shuffle', default=dataloader_params.shuffle, type=bool, help='Shuffle the dataset during evaluation')
+# @click.option('--num-workers', default=dataloader_params.num_workers, type=int, help='Number of workers for data loading during evaluation')
+# @click.option('--pin-memory', default=dataloader_params.pin_memory, type=bool, help='Use pinned memory for data loading during evaluation')
 def evaluate(checkpoint_path, 
              data_dir,
              batch_size,
@@ -17,15 +36,18 @@ def evaluate(checkpoint_path,
              num_workers,
              pin_memory):
     
+    if not Path(checkpoint_path).exists():
+        raise FileNotFoundError(f"The checkpoint path {checkpoint_path} does not exist.")
+        
     prot_cnn_loaded = ProtCNN.load_from_checkpoint(checkpoint_path=checkpoint_path)
     
     # Defining the datamodule
-    datamodule = DataModule(data_dir=data_dir,
-                            batch_size=batch_size,
-                            max_len=max_len,
-                            shuffle=shuffle,
-                            num_workers=num_workers,
-                            pin_memory=pin_memory)
+    datamodule = DataModule(data_dir=prot_cnn_loaded.hparams.data_dir,
+                            batch_size=prot_cnn_loaded.hparams.batch_size,
+                            max_len=prot_cnn_loaded.hparams.max_len,
+                            shuffle=prot_cnn_loaded.hparams.shuffle,
+                            num_workers=prot_cnn_loaded.hparams.num_workers,
+                            pin_memory=prot_cnn_loaded.hparams.pin_memory)
     
     # Defining the trainer
     trainer = Trainer()
@@ -38,32 +60,5 @@ def evaluate(checkpoint_path,
 
 if __name__ == "__main__":
     
-    root = Path(__file__).parent.parent
-    json_path = root / 'params.json'
-
-    with open(json_path, 'r') as file:
-            params = json.load(file, object_hook=lambda d: SimpleNamespace(**d))
-            
-    # Getting the checkpoint_path
-    evaluation_params = params.evaluation
-    checkpoint_path = evaluation_params.checkpoint_path
-    
-    # Getting dataloader parameters
-    dataloader_params = params.dataloader
-    
-    data_dir = root / dataloader_params.data_dir
-    batch_size = dataloader_params.batch_size
-    max_len = dataloader_params.max_len
-    shuffle = dataloader_params.shuffle
-    num_workers = dataloader_params.num_workers
-    pin_memory = dataloader_params.pin_memory
-    
-    if Path(checkpoint_path).exists:
-        evaluation_results = evaluation_results = evaluate(checkpoint_path=checkpoint_path,
-                                    data_dir=data_dir,
-                                    batch_size=batch_size,
-                                    max_len=max_len,
-                                    shuffle=shuffle,
-                                    num_workers=num_workers,
-                                    pin_memory=pin_memory)
+        evaluation_results = evaluate()
         print(evaluation_results)
